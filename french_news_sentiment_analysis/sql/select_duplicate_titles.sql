1ff108588a46f018b0954f1d67fc4f61df62ceb8
PetitSamuel <petits@tcd.ie>
2021-04-21 23:40:53 +0200

    feel stat data

--- french_news_sentiment_analysis/sql/select_duplicate_titles.sql
+++ french_news_sentiment_analysis/sql/select_duplicate_titles.sql
@@ -7,3 +7,22 @@ FROM articles a
         GROUP BY title
         HAVING COUNT(title) > 1
     ) dup ON a.title = dup.title;
+-- SELECT 
+--     articles.id, articles.title, articles.source
+-- FROM 
+--     articles
+-- INNER JOIN 
+--     articles from_source ON (from_source.title = articles.title)
+-- WHERE 
+--     from_source.source = articles.source;
+-- select 
+--     length,
+--     feel_sentiment_positive,
+--     feel_sentiment_negative,
+--     feel_joy,
+--     feel_fear,
+--     feel_sadness,
+--     feel_anger,
+--     feel_surprise,
+--     feel_disgust
+-- from articles INTO OUTFILE '/var/lib/mysql-files/feel_stat_overview.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
