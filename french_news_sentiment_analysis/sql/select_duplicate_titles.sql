1d4c6d9e1fb0b95719d5fe3a3d4264a697b8748f
PetitSamuel <petits@tcd.ie>
2021-04-24 12:24:05 +0200

    updating csvs and adding lit review material

--- french_news_sentiment_analysis/sql/select_duplicate_titles.sql
+++ french_news_sentiment_analysis/sql/select_duplicate_titles.sql
@@ -15,14 +15,41 @@ FROM articles a
 --     articles from_source ON (from_source.title = articles.title)
 -- WHERE 
 --     from_source.source = articles.source;
--- select 
---     length,
+-- select length,
 --     feel_sentiment_positive,
 --     feel_sentiment_negative,
---     feel_joy,
---     feel_fear,
---     feel_sadness,
---     feel_anger,
---     feel_surprise,
---     feel_disgust
--- from articles INTO OUTFILE '/var/lib/mysql-files/feel_stat_overview.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+--     polarimots_positive,
+--     polarimots_negative,
+--     diko_positive,
+--     diko_negative
+-- from articles INTO OUTFILE '/var/lib/mysql-files/monthlysentiment.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+-- select month(date),
+--     year(date),
+--     sum(length),
+--     avg(feel_sentiment_positive / length),
+--     stddev(feel_sentiment_positive / length),
+--     avg(feel_sentiment_negative / length),
+--     stddev(feel_sentiment_negative / length),
+--     avg(feel_joy / length),
+--     stddev(feel_joy / length),
+--     avg(feel_fear / length),
+--     stddev(feel_fear / length),
+--     avg(feel_sadness / length),
+--     stddev(feel_sadness / length),
+--     avg(feel_anger / length),
+--     stddev(feel_anger / length),
+--     avg(feel_surprise / length),
+--     stddev(feel_surprise / length),
+--     avg(feel_disgust / length),
+--     stddev(feel_disgust / length),
+--     avg(death_sentiment / length),
+--     stddev(death_sentiment / length),
+--     avg(virus_sentiment / length),
+--     stddev(virus_sentiment / length),
+--     avg(vaccine_sentiment / length),
+--     stddev(vaccine_sentiment / length)
+-- from articles
+-- group by month(date),
+--     year(date)
+-- order by year(date),
+--     month(date) INTO OUTFILE '/var/lib/mysql-files/monthlysentiment.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
